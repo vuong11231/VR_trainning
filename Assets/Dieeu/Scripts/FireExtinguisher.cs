aac3850f793a4d8ffe62f27eca778999d755ae80
Hân <hanphan1509@gmail.com>
2024-03-28 14:26:35 +0700

    add models

---
+++ Assets/Dieeu/Scripts/FireExtinguisher.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireExtinguisher : MonoBehaviour
+{
+    public void GetFireExtinguisher()
+    {
+
+    }
+
+    public void PullingThePin()
+    {
+
+    }
+
+    public void SqueezeTheLever()
+    {
+
+    }
+
+    public void ReleasingTheLever()
+    {
+
+    }    
+
+}
